d92bf48e8c5e99d5508325c0d674d1aa597d622a
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-01-28 16:33:07 +0900

    Added App class. Included render of the initial content on the page.

---
+++ public/classes/App.js
@@ -0,0 +1,24 @@
+/**
+ * App
+ *
+ * This class handles the render of the HTML at the root level.
+ * It manages the app state and sends the data to the different
+ * Web Components
+ */
+export class App {
+	constructor() {
+		this.$root = document.getElementById("root");
+
+		this.render();
+	}
+
+	/**
+	 * Render the component HTML in its Shadow DOM
+	 */
+	render() {
+		this.$root.innerHTML = `
+			<main id="content">
+			</main>
+		`;
+	}
+}
